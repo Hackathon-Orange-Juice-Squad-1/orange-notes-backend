3c7fdb115529ed43c12219da1c0b61c89ab280e3
Kaique Vieira <kaiquevieira329@gmail.com>
2024-01-25 12:22:30 -0300

    FEAT: Primeiros controllers de usuário criados

--- src/app.js
+++ src/app.js
@@ -1,32 +1,19 @@
 require('dotenv').config()
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
-const userRoute = require('./routes/User')
+const userRoute = require('./routes/userRoutes')
 const MONGO_URL = process.env.MONGO_URL
 
-app.use(
-    express.urlencoded({
-        extended:true,
-    }),
-)
-
+app.use(express.urlencoded({extended:true}))
 app.use(express.json()) //
 
 // Routes
 app.use('/auth', userRoute)
 
 // Conexão com o bando de dados
-mongoose
-    .connect(
-        MONGO_URL
-    )
-    //função anônima
-    .then(() =>{
-        console.log('Conectamos')
+mongoose.connect(MONGO_URL).then(() =>{
+        console.log('Conectado ao BD com sucesso! http://localhost:3000')
         app.listen(3000)
-    })
-    .catch((err) => console.log(err))
+    }).catch((err) => console.log(err))
